165c87d4402a36c66ab772fcd2742ca2361a64e7
jpda <john.dandison@live.com>
2023-11-22 11:43:07 -0500

    work aliases

--- zsh/paths-aliases.zsh
+++ zsh/paths-aliases.zsh
@@ -2,6 +2,9 @@
 export PATH="$PATH:$HOME/.dotnet/tools:$HOME/.local/bin:$HOME/Library/Python/3.10/bin"
 export PATH="$PATH:$HOME/.local/platform-tools"
 
+pwd_alias() { echo "$PWD"; }
+loadenv() { source $(pwd_alias)/**/*.Development.env; }
+
 command -v lsd &>/dev/null && alias ls='lsd --group-dirs first'
 command -v gotop &>/dev/null && alias top='gotop -p'
 alias l='ls -l'
@@ -13,3 +16,8 @@ alias today='events -n eventsToday'
 alias upnext='events -n -li 1 eventsToday'
 alias tomorrow='events eventsFrom:"tomorrow" to:"tomorrow"'
 alias refreshOfficeTemplates='cp -r $OneDriveConsumer/Documents/Custom\ Office\ Templates/ ~/Library/Group\ Containers/UBF8T346G9.Office/User\ Content.localized/Templates.localized'
+
+alias workenv="cd $WORK_ROOT && loadenv"
+alias isworkenv="cd $OIS_ROOT && loadenv"
+alias runis='isworkenv && dotnet run -c Release'
+alias hostis='ngrok --config $HOME/.local/etc/ngrok.yml,$OIS_ROOT/ngrok.yml start ois'
