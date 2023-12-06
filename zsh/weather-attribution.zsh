316660a2006994dc25f468e765864d390bdaae5e
jpda <john.dandison@live.com>
2023-12-06 12:33:06 -0500

    minor weather change

---
+++ zsh/weather-attribution.zsh
@@ -0,0 +1,5 @@
+# /bin/zsh
+# apple weather attribution requirements 🙄
+
+#echo "Weather data provided by \ue711 $(curl -s "https://weatherkit.apple.com/attribution/en" | jq -r '.serviceName')"
+echo "Weather data provided by \ue711 Weather"
